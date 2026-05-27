71df83e524d220624913d8a0e4ed7c450fa42ad1
KWANSIUWING <skwa0013@student.monash.edu>
2026-05-27 20:08:52 +0800

    Added new VIZ

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -7,3 +7,8 @@ var vg_2 = "V2_1.vg.json";
 vegaEmbed("#line_chart1", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "V2_2.vg.json";
+vegaEmbed("#scatter_plot1", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
